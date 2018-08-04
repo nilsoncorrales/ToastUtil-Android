9ece0577fd0a65af9535fb42dd333d1d71f857c1
victor <n_ezequiel98@hotmail.com>
2018-08-04 10:08:38 -0500

    Create project and library ToasUtil

---
+++ toastutil/src/main/java/printer/com/citectra/toastutil/ToastUtil.java
@@ -0,0 +1,14 @@
+package printer.com.citectra.toastutil;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+    
+    public static void toast(Context context, String msg){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
+}
